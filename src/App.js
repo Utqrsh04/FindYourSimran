3e94aff5108ed637f97ac19363aa7a3cf84c2106
Anurodh437 <anurodh.dubey_cs19@ga.ac.in>
2021-09-11 20:11:50 +0530

    added a navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import Authpage from "./Components/Auth/Authpage.jsx";
+import Navbar from "./Components/navigation/navbar";
 
 function App() {
   return (
     <div className="App">
+      {/* <Navbar /> */}
       <Authpage/>
     </div>
   );
