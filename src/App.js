8678cd49f0bfa32809d6df62fd4dd1aa3ed9b9b9
Utqrsh04 <utkarshrkt2001@gmail.com>
2021-09-18 09:53:02 +0530

    ❌  Removed Particle.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
-// import "./App.css";
-import Particle from "./Components/Particles";
 import Authpage from "./Pages/Auth/Authpage.jsx";
-// import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
-// import Dashboard from "./Pages/AppContainer/Dashboard";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
+import Dashboard from "./Pages/AppContainer/Dashboard";
 // import Login from "./Pages/Auth/Loginpage";
 
 function App() {
   return (
-    <div className="App bg-none max-h-screen overflow-hidden ">
-      <Particle />
-      <div className="relative bg-none -top-60">
-        <Authpage />
-      </div>
-      {/* <Router>
-      <Switch>
-        <Route exact path="/">
-         <Authpage/>
-        </Route>
-        <Route exact path="/login">
-         <Authpage/>
-        </Route>
-        <Route exact path="/dashboard">
-          <Dashboard/>
-        </Route>
-      </Switch>
-    </Router> */}
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Authpage />
+          </Route>
+          <Route exact path="/login">
+            <Authpage />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
