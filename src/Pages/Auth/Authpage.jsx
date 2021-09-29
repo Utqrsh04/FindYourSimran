63945adc6e67880d113c1f3b3b4cc2457ffedd95
Utqrsh04 <utkarshrkt2001@gmail.com>
2021-09-29 22:05:49 +0530

    Added Classes to Auth Page , Made Signup page for Small screens

--- src/Pages/Auth/Authpage.jsx
+++ src/Pages/Auth/Authpage.jsx
@@ -4,16 +4,31 @@ import { FcGoogle } from "react-icons/fc";
 import { SiGithub, SiLinkedin } from "react-icons/si";
 
 export default function Authpage() {
-  const [SignUp, setSignUp] = useState(false);
+  const [show, setShow] = useState(false);
   const SignInClicked = () => {
-    setSignUp(!SignUp);
+    setShow(!show);
   };
   const SignUpClicked = () => {
-    setSignUp(!SignUp);
+    setShow(!show);
   };
 
   let classnames = " ";
-  if (SignUp) classnames = " right-panel-active ";
+  if (show) classnames = " right-panel-active ";
+
+  const [hide, Sethide] = useState(false);
+  const [signInClass, setSignInClass] = useState("bg-blue-600");
+  const [signUpClass, setSignUpClass] = useState("bg-red-400");
+  const OnsignUp = () => {
+    setSignInClass(" bg-red-400 ");
+    setSignUpClass(" bg-blue-400 ");
+    Sethide(true)
+  };
+
+  const OnsignIn = () => {
+    setSignInClass(" bg-blue-400 ");
+    setSignUpClass(" bg-red-400 ");
+  };
+  console.log("Sign in :", signInClass, " Sign up :", signUpClass);
 
   return (
     <div className=" bgImage flex justify-center items-center h-screen">
@@ -97,27 +112,28 @@ export default function Authpage() {
         </div>
       </div>
 
+      {/* for sm/md screens */}
       <div className="h-screen w-full block md:hidden">
-        <div className="bg-white mx-auto mt-20 w-5/6 h-5/6 rounded-lg">
-          <div className="flex items-center justify-center w-full pt-8">
-            <label
-              htmlFor="toggleB"
-              className="flex items-center cursor-pointer"
-            >
-              <div className="relative">
-                <input className="inp" type="checkbox" id="toggleB" className="sr-only" />
-                <div className="block bg-blue-500 w-36 h-10 rounded-full font-bold text-sm pr-2 text-right pt-2.5">
-                  Sign Up
-                </div>
-                <div
-                  className="dot absolute left-1 top-1 bg-white  w-16 h-8 rounded-full 
-                font-bold text-sm flex justify-center items-center transition"
-                >
-                  Sign In
-                </div>
+        <div className="bg-white mx-auto mt-20 w-5/6 h-5/6 rounded-xl">
+          {/* Toggle btns */}
+          <div className="flex items-center justify-center w-full pt-5">
+            <div className="flex bg-red-400 px-1 py-1 rounded-full transition-colors  ease-in-out">
+              <div
+                onClick={OnsignIn}
+                className={`text-white h-10 w-20 rounded-full font-bold text-sm pr-2 flex justify-center align-middle pt-2.5 ${signInClass} `}
+              >
+                Sign In
               </div>
-            </label>
+              <div
+                onClick={OnsignUp}
+                className={`text-white h-10 w-20 rounded-full font-bold text-sm pr-2 flex justify-center align-middle pt-2.5 ${signUpClass} `}
+              >
+                Sign Up
+              </div>
+            </div>
           </div>
+
+          {/* Sign In */}
           <div className="flex flex-col justify-center items-center mt-6 ">
             <h1 className="font-bold text-xl text-center heading">Sign In</h1>
             <div className=" space-x-5 mt-10 mb-5 flex justify-center mx-auto ">
@@ -160,6 +176,57 @@ export default function Authpage() {
           </h1>
           <div className="flex justify-center mt-8">
             <button className="btn">Sign In</button>
+          </div>
+
+          {/* Sign Up */}
+          <div className=" flex flex-col justify-center items-center mt-6 " >
+            <h1 className="font-bold text-lg text-center heading">Sign Up</h1>
+            <div className=" space-x-5 mt-5 mb-5 flex justify-center mx-auto ">
+              <a href=" " className="social links text-xl">
+                <SiGithub />
+              </a>
+              <a href=" " className="social links text-xl">
+                <FcGoogle />
+              </a>
+              <a href=" " className="social links text-xl">
+                <SiLinkedin />
+              </a>
+            </div>
+            <h1 className="text-sm font-medium heading">Or use your account</h1>
+          </div>
+
+          <div className="mx-8 mt-7 space-y-5">
+            <div>
+              <input
+                className="inp"
+                type="email"
+                name=""
+                id="email"
+                placeholder="Email"
+              />
+            </div>
+            <div>
+              <input
+                className="inp"
+                type="text"
+                name=""
+                id="name"
+                placeholder="Fullname"
+              />
+            </div>
+
+            <div>
+              <input
+                className="inp"
+                type="password"
+                name=""
+                id="password"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center mt-8">
+            <button className="btn">Sign Up</button>
           </div>
         </div>
       </div>
