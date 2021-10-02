fac7e5da9f07f25852eecf9c57433a7dd79c9d63
Utqrsh04 <utkarshrkt2001@gmail.com>
2021-10-02 16:38:21 +0530

    ✨ Feat: Added Loader and Toast Component

--- src/App.js
+++ src/App.js
@@ -7,29 +7,40 @@ import {
 } from "react-router-dom";
 import Dashboard from "./Pages/AppContainer/Dashboard";
 import NotFound from "./Pages/AppContainer/NotFound.jsx";
+import Loader from "./Components/Loader/Loader.jsx";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [showLoader, setShowLoader] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setShowLoader(false), 2000);
+  }, []);
+
   return (
     <div>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route exact path="/login">
-            <Authpage />
-          </Route>
-          <Route exact path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/notFound" exact>
-            <NotFound />
-          </Route>
-          <Route>
-            <Redirect to="/notFound" />
-          </Route>
-        </Switch>
-      </Router>
+      {showLoader === true && <Loader />}
+      {showLoader === false && (
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/login" />
+            </Route>
+            <Route exact path="/login">
+              <Authpage />
+            </Route>
+            <Route exact path="/dashboard">
+              <Dashboard />
+            </Route>
+            <Route path="/notFound" exact>
+              <NotFound />
+            </Route>
+            <Route>
+              <Redirect to="/notFound" />
+            </Route>
+          </Switch>
+        </Router>
+      )}
     </div>
   );
 }
